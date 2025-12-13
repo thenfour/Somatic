23737802b71a6da57f101c601fc768f8722dad60
tenfour <carlco@gmail.com>
2025-12-13 01:47:13 +0100

    tenfour title

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Chromatic',
+      title: 'Chromatic (tenfour edition)',
       template: 'index.html',
     }),
     new MiniCssExtractPlugin(),
