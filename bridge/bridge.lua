8c7ce59f6add04e4272ce750e68de72f95e92af3
tenfour <carlco@gmail.com>
2026-01-05 15:45:29 +0100

    back to using sweetie16 in tic80 embed

--- bridge/bridge.lua
+++ bridge/bridge.lua
@@ -1401,6 +1401,23 @@ function tf_music_tick()
 	swapInPlayorder(currentSongOrder, destPointer)
 end
 
+local sweetie16_pal = "1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57"
+
+-- set the palette
+function tovram(str)
+	local o = 0
+	for c = 1, #str, 2 do -- walk colors
+		local v = tonumber(str:sub(c, c + 1), 16) -- get color (v)alue
+		poke(0x3fc0 + o, v)
+		o = o + 1 -- set color
+	end
+end
+
+tovram(sweetie16_pal)
+poke(0x3FF8, 0) -- border
+poke(0x3FF9, 0) -- screen offset
+poke(0x3FFA, 0)
+
 -- =========================
 -- TIC loop
 -- =========================
